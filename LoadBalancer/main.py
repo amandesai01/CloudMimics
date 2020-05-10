97b0c857708551dd40b26a57a98cfba60d9f5ac4
Aman Desai <amandesai01@gmail.com>
2020-05-10 17:45:02 +0530

    Major Working and Redirects added - #LoadBalancer

--- LoadBalancer/main.py
+++ LoadBalancer/main.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, redirect
-from .Register import register
+from flask import Flask, redirect
+import pickle
+
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def balancer(path):
-    print(path)
-    return "<h1>Work in Progress</h1>"
-
+    register = pickle.load(open('register.pkl', 'rb'))
+    url = register.get_min_loaded_server()['url']
+    pickle.dump(register, open('register.pkl', 'wb'))
+    return redirect(url)
+    
 if __name__ == '__main__':
     app.run(port=4999, debug=True)
     
