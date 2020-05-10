b0ec2d6d091951d35b15ffc7b6fbb33874273fed
Aman Desai <amandesai01@gmail.com>
2020-05-10 18:04:07 +0530

    Added Server Check Function - #LoadBalancer

--- LoadBalancer/main.py
+++ LoadBalancer/main.py
@@ -1,7 +1,19 @@
 from flask import Flask, redirect
 import pickle
+from ping3 import ping
+import time
 
 app = Flask(__name__)
+
+def check_servers():
+    while True:
+        register = pickle.load(open('register.pkl', 'rb'))
+        servers = register.list_servers
+        for server in servers:
+            if not ping(server['url']):
+                server['is_active'] = False
+        pickle.dump(register, open('register.pkl', 'wb'))
+        time.sleep(2.5)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
