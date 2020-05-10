0e7d58df1faac3593fcd7e439bfe498d18945582
Aman Desai <amandesai01@gmail.com>
2020-05-10 17:12:02 +0530

    Refactored Register. Deprecated Heaps

--- LoadBalancer/Register/register.py
+++ LoadBalancer/Register/register.py
@@ -1,42 +1,36 @@
-import json
-from .heap import Heap 
-import pickle
-def register_server():
-    '''
-    We Save All server instances here.
-    Currently, Manually visit this function and append.
-    A seperate endpoint to be exposed later.
-    Currently , this returns a list of servers ordered in ascending order based on server load. 
-    '''
-    heap = Heap(10000)
-    list_servers = pickle.load(open('server_list.pkl','rb'))
-    list_servers.append({
-        "url":"http://127.0.0.1:5000",
-        "current_load":0,
-        "is_active":True,
-        "total_requests_served":0
-    })
-    heap.add(list_servers)
-    heap.minHeap()
-    list_servers = heap.heap
-    pickle.dump(list_servers,open('server_list.pkl','wb'))
+import sys
 
-def get_server_list():
-    '''
-    This Returns Port Number of Server Instance with minimum load. 
-    '''
-    return pickle.load(open('server_list.pkl','rb'))
+class Register():
+    def __init__(self):
+        self.list_servers = list()
+        self.min_loaded_server = dict()
     
+    def register_server(self):
+        '''
+        We Save All server instances here.
+        Currently, Manually visit this function and append.
+        A seperate endpoint to be exposed later.
+        Currently , this returns a list of servers ordered in ascending order based on server load. 
+        '''
+        list_servers.append({
+            "url":"http://127.0.0.1:5000",
+            "is_active":True,
+            "total_requests_served":0
+        })
 
-def min_loaded_server():
-    '''
-    This method will act as entry point while exposing endpoint to add servers.
-    To be left blank for now.
-    '''
-    return get_server_list()[0]
+        self.update_min()
+        return
 
-def update_pickle():
-    '''
-    This method will re-pickelize server list, since we will have modified serve count, uptime, etc.
-    '''
-    pass
+    def reset_servers(self):
+        for server in self.list_servers:
+            server['total_requests_served'] = 0
+        return
+
+    def update_min(self):
+        for server in self.list_servers:
+            if server['total_requests_served'] < sys.maxsize:
+                self.min_loaded_server = server
+        return
+    
+    def get_min_loaded_server(self):
+        return self.min_loaded_server
