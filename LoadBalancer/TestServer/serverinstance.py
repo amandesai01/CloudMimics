bc1ba56e55dca2a44d4200280aa4737ca43f6273
Aman Desai <amandesai01@gmail.com>
2020-05-17 22:45:10 +0530

    ServerInstance Basic Config

--- LoadBalancer/TestServer/serverinstance.py
+++ LoadBalancer/TestServer/serverinstance.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify
 from time import sleep
+import threading
 
 app = Flask(__name__)
 
+PORT = 5005
+
 @app.route('/')
 def route():
+    '''
+    Here we assume that each server takes 0.5 seconds to respond.
+    '''
     sleep(0.5)
     return jsonify({"status" : "served"})
+
+if __name__ == '__main__':
+    app.run(port=PORT)
